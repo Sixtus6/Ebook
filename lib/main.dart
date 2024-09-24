a2b3c0695c07e3383e3da0c38f515f00959f9894
Abasiono Eton (Sixtus) <72328675+Sixtus6@users.noreply.github.com>
2024-09-24 12:46:01 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+//https://github.com/bmdavis419/the-better-backend
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
